f7de77c120a69e2477435ff290f8c26b4418f07c
erlgithub88 <ea490569@gmail.com>
2024-10-23 10:40:33 +0700

    fitur upload

--- function.php
+++ function.php
@@ -45,19 +45,28 @@ function ubah_tamu($data)
 {
     global $koneksi;
 
-    $id         = htmlspecialchars($data["id_tamu"]);
-    $nama_tamu  = htmlspecialchars($data["nama_tamu"]);
-    $alamat     = htmlspecialchars($data["alamat"]);
-    $no_hp      = htmlspecialchars($data["no_hp"]);
-    $bertemu    = htmlspecialchars($data["bertemu"]);
+    $id          = htmlspecialchars($data["id_tamu"]);
+    $nama_tamu   = htmlspecialchars($data["nama_tamu"]);
+    $alamat      = htmlspecialchars($data["alamat"]);
+    $no_hp       = htmlspecialchars($data["no_hp"]);
+    $bertemu     = htmlspecialchars($data["bertemu"]);
     $kepentingan = htmlspecialchars($data["kepentingan"]);
+    $gambarLama  = htmlspecialchars($data['gambarLama']);
 
+    //cek apakah user pilih gambar baru atau tidak
+    if ($_FILES['gambar']['error'] === 4) {
+        $gambar = $gambarLama;
+    }else {
+        $gambar = uploadGambar();
+    }
+    
     $query = "UPDATE buku_tamu SET
                 nama_tamu   = '$nama_tamu',
                 alamat      = '$alamat',
                 no_hp       = '$no_hp',
                 bertemu     = '$bertemu',
                 kepentingan = '$kepentingan'
+                gambar      = '$gambar'
                 WHERE id_tamu = '$id'";
 
     mysqli_query($koneksi, $query);
@@ -145,7 +154,7 @@ function uploadGambar()
     //ambil data file gambar dari variable $_FILES
     $namaFile = $_FILES['gambar'] ['name'];
     $ukuranFile = $_FILES['gambar'] ['size'];
-    $error = $_FILES['gambar'] ['eror'];
+    $error = $_FILES['gambar'] ['error'];
     $tmpName = $_FILES['gambar'] ['tmp_name'];
 
     //cek apakah tidak ada gambar yang diunggah
